4b0c5afe84784c1ccf37b4a5c9be6bc46c442386
hola@jorgemht.dev <hola@jorgemht.dev>
2018-09-24 09:44:01 +0200

    Inital commit. Basic test passed.

---
+++ Jshop.Backend/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Jshop.Backend.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
